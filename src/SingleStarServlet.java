2c803bd10aec7d2e3ea21e3a801cb588e87ddd8e
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-04-08 11:47:22 -0700

    update to work with java 11

--- src/SingleStarServlet.java
+++ src/SingleStarServlet.java
@@ -1,8 +1,9 @@
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-import javax.annotation.Resource;
-import javax.servlet.ServletException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,91 +18,101 @@ import java.sql.ResultSet;
 // Declaring a WebServlet called SingleStarServlet, which maps to url "/api/single-star"
 @WebServlet(name = "SingleStarServlet", urlPatterns = "/api/single-star")
 public class SingleStarServlet extends HttpServlet {
-	private static final long serialVersionUID = 2L;
+    private static final long serialVersionUID = 2L;
 
-	// Create a dataSource which registered in web.xml
-	@Resource(name = "jdbc/moviedbexample")
-	private DataSource dataSource;
+    // Create a dataSource which registered in web.xml
+    private DataSource dataSource;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
 
-		response.setContentType("application/json"); // Response mime type
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		// Retrieve parameter id from url request.
-		String id = request.getParameter("id");
+        response.setContentType("application/json"); // Response mime type
 
-		// Output stream to STDOUT
-		PrintWriter out = response.getWriter();
+        // Retrieve parameter id from url request.
+        String id = request.getParameter("id");
 
-		try {
-			// Get a connection from dataSource
-			Connection dbcon = dataSource.getConnection();
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
 
-			// Construct a query with parameter represented by "?"
-			String query = "SELECT * from stars as s, stars_in_movies as sim, movies as m where m.id = sim.movieId and sim.starId = s.id and s.id = ?";
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try (Connection conn = dataSource.getConnection()) {
+            // Get a connection from dataSource
 
-			// Declare our statement
-			PreparedStatement statement = dbcon.prepareStatement(query);
+            // Construct a query with parameter represented by "?"
+            String query = "SELECT * from stars as s, stars_in_movies as sim, movies as m " +
+                    "where m.id = sim.movieId and sim.starId = s.id and s.id = ?";
 
-			// Set the parameter represented by "?" in the query to the id we get from url,
-			// num 1 indicates the first "?" in the query
-			statement.setString(1, id);
+            // Declare our statement
+            PreparedStatement statement = conn.prepareStatement(query);
 
-			// Perform the query
-			ResultSet rs = statement.executeQuery();
+            // Set the parameter represented by "?" in the query to the id we get from url,
+            // num 1 indicates the first "?" in the query
+            statement.setString(1, id);
 
-			JsonArray jsonArray = new JsonArray();
+            // Perform the query
+            ResultSet rs = statement.executeQuery();
 
-			// Iterate through each row of rs
-			while (rs.next()) {
+            JsonArray jsonArray = new JsonArray();
 
-				String starId = rs.getString("starId");
-				String starName = rs.getString("name");
-				String starDob = rs.getString("birthYear");
+            // Iterate through each row of rs
+            while (rs.next()) {
 
-				String movieId = rs.getString("movieId");
-				String movieTitle = rs.getString("title");
-				String movieYear = rs.getString("year");
-				String movieDirector = rs.getString("director");
+                String starId = rs.getString("starId");
+                String starName = rs.getString("name");
+                String starDob = rs.getString("birthYear");
 
-				// Create a JsonObject based on the data we retrieve from rs
+                String movieId = rs.getString("movieId");
+                String movieTitle = rs.getString("title");
+                String movieYear = rs.getString("year");
+                String movieDirector = rs.getString("director");
 
-				JsonObject jsonObject = new JsonObject();
-				jsonObject.addProperty("star_id", starId);
-				jsonObject.addProperty("star_name", starName);
-				jsonObject.addProperty("star_dob", starDob);
-				jsonObject.addProperty("movie_id", movieId);
-				jsonObject.addProperty("movie_title", movieTitle);
-				jsonObject.addProperty("movie_year", movieYear);
-				jsonObject.addProperty("movie_director", movieDirector);
+                // Create a JsonObject based on the data we retrieve from rs
 
-				jsonArray.add(jsonObject);
-			}
-			
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("star_id", starId);
+                jsonObject.addProperty("star_name", starName);
+                jsonObject.addProperty("star_dob", starDob);
+                jsonObject.addProperty("movie_id", movieId);
+                jsonObject.addProperty("movie_title", movieTitle);
+                jsonObject.addProperty("movie_year", movieYear);
+                jsonObject.addProperty("movie_director", movieDirector);
+
+                jsonArray.add(jsonObject);
+            }
+            rs.close();
+            statement.close();
+
             // write JSON string to output
             out.write(jsonArray.toString());
             // set response status to 200 (OK)
             response.setStatus(200);
 
-			rs.close();
-			statement.close();
-			dbcon.close();
-		} catch (Exception e) {
-			// write error message JSON object to output
-			JsonObject jsonObject = new JsonObject();
-			jsonObject.addProperty("errorMessage", e.getMessage());
-			out.write(jsonObject.toString());
+        } catch (Exception e) {
+            // write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
 
-			// set reponse status to 500 (Internal Server Error)
-			response.setStatus(500);
-		}
-		out.close();
+            // set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
 
-	}
+        // always remember to close db connection after usage. Here it's done by try-with-resources
+
+
+    }
 
 }
--- src/StarsServlet.java
+++ src/StarsServlet.java
@@ -1,8 +1,9 @@
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-import javax.annotation.Resource;
-import javax.servlet.ServletException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,26 +21,32 @@ import java.sql.Statement;
 public class StarsServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    // Create a dataSource which registered in web.xml
-    @Resource(name = "jdbc/moviedbexample")
+    // Create a dataSource which registered in web.
     private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         response.setContentType("application/json"); // Response mime type
 
         // Output stream to STDOUT
         PrintWriter out = response.getWriter();
 
-        try {
-            // Get a connection from dataSource
-            Connection dbcon = dataSource.getConnection();
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try (Connection conn = dataSource.getConnection()) {
 
             // Declare our statement
-            Statement statement = dbcon.createStatement();
+            Statement statement = conn.createStatement();
 
             String query = "SELECT * from stars";
 
@@ -62,27 +69,28 @@ public class StarsServlet extends HttpServlet {
 
                 jsonArray.add(jsonObject);
             }
-            
+            rs.close();
+            statement.close();
+
             // write JSON string to output
             out.write(jsonArray.toString());
             // set response status to 200 (OK)
             response.setStatus(200);
 
-            rs.close();
-            statement.close();
-            dbcon.close();
+
         } catch (Exception e) {
-        	
-			// write error message JSON object to output
-			JsonObject jsonObject = new JsonObject();
-			jsonObject.addProperty("errorMessage", e.getMessage());
-			out.write(jsonObject.toString());
 
-			// set reponse status to 500 (Internal Server Error)
-			response.setStatus(500);
+            // write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
 
+            // set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
         }
-        out.close();
+        // always remember to close db connection after usage. Here it's done by try-with-resources
 
     }
 }
